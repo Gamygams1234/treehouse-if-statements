f7250bcc4207ce0e173f916eb5d435c3d834891d
Gamygams1234 <gamyburgos@gmail.com>
2020-06-05 22:21:34 -0700

    My solution to the issue

--- js/math.js
+++ js/math.js
@@ -2,23 +2,40 @@
 
 // announce the program
 alert("Let's do some math!");
+var num1;
+var num2;
 
 // collect numeric input
-var num1 = prompt("Please type a number");
-num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
-num2 = parseFloat(num2);
+function checkNum1() {
+  num1 = prompt("Please type a number");
+  num1 = parseFloat(num1);
+  if (Number.isNaN(num1)) {
+    checkNum1();
+  } else {
+    checkNum2();
+  }
+}
+
+function checkNum2() {
+  num2 = prompt("Please type another number which is not zero");
+  num2 = parseFloat(num2);
+  if (Number.isNaN(num2)) {
+    checkNum2();
+  } else if (num2 == 0) {
+    checkNum2();
+  }
+}
+checkNum1();
 
 // build an HTML message
 var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 message += num1 + " + " + num2 + " = " + (num1 + num2);
 message += "<br>";
-message += num1 + " * " + num2 + " = " + (num1 * num2);
+message += num1 + " * " + num2 + " = " + num1 * num2;
 message += "<br>";
-message += num1 + " / " + num2 + " = " + (num1 / num2);
+message += num1 + " / " + num2 + " = " + num1 / num2;
 message += "<br>";
 message += num1 + " - " + num2 + " = " + (num1 - num2);
-  
+
 // write message to web page
 document.write(message);
-
